175f05119047b7819146d368a693aaf7138b2ae7
Jonathan Nilsson <jonathan@voysys.se>
2022-10-25 00:02:34 +0200

    Weeee

--- app/src/main/java/dev/jonil/leds_control/LedControler.kt
+++ app/src/main/java/dev/jonil/leds_control/LedControler.kt
@@ -1,33 +1,47 @@
 package dev.jonil.leds_control
 
+import android.app.PendingIntent
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
+import android.content.ComponentName
 import android.content.Context
+import android.content.Intent
+import android.util.Log
 import android.widget.RemoteViews
 
+
 class LedControler : AppWidgetProvider() {
+
+    var tag = "main"
+
     override fun onUpdate(
         context: Context,
         appWidgetManager: AppWidgetManager,
         appWidgetIds: IntArray
     ) {
         for (appWidgetId in appWidgetIds) {
-            updateAppWidget(context, appWidgetManager, appWidgetId)
+            Log.w(tag, String.format("appWidgetId %d", appWidgetId))
+
+            val remoteViews = RemoteViews(context.packageName, R.layout.led_controler)
+            remoteViews.setOnClickPendingIntent(R.id.on_button, getPendingSelfIntent(context, "ON"))
+            remoteViews.setOnClickPendingIntent(R.id.off_button, getPendingSelfIntent(context, "OFF"))
+            remoteViews.setOnClickPendingIntent(R.id.red_button, getPendingSelfIntent(context, "RED"))
+
+            appWidgetManager.updateAppWidget(appWidgetId, remoteViews)
         }
     }
 
-    override fun onEnabled(context: Context) {
+    private fun getPendingSelfIntent(context: Context?, action: String?): PendingIntent? {
+        val intent = Intent(context, javaClass)
+        intent.action = action
+        return PendingIntent.getBroadcast(context, 0, intent, 0)
     }
 
-    override fun onDisabled(context: Context) {
-    }
-}
+    override fun onEnabled(context: Context) {}
+    override fun onDisabled(context: Context) {}
 
-internal fun updateAppWidget(
-    context: Context,
-    appWidgetManager: AppWidgetManager,
-    appWidgetId: Int
-) {
-    val views = RemoteViews(context.packageName, R.layout.led_controler)
-    appWidgetManager.updateAppWidget(appWidgetId, views)
+    override fun onReceive(context: Context?, intent: Intent?) {
+        super.onReceive(context, intent)
+        Log.d(tag, intent!!.action!!)
+    }
 }

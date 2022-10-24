d1dce271768468390db4b101d454b3e3eebcaf93
Jonathan Nilsson <jonathan@voysys.se>
2022-10-25 00:17:18 +0200

    Simpler

--- app/src/main/java/dev/jonil/leds_control/LedControler.kt
+++ app/src/main/java/dev/jonil/leds_control/LedControler.kt
@@ -3,12 +3,10 @@ package dev.jonil.leds_control
 import android.app.PendingIntent
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
-import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
 import android.util.Log
 import android.widget.RemoteViews
-
 
 class LedControler : AppWidgetProvider() {
 

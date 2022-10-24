f687db33b3a64a518c3a373fb49d6cf474025533
Jonathan Nilsson <jonathan@voysys.se>
2022-10-25 01:02:38 +0200

    Client

--- app/src/main/java/dev/jonil/leds_control/LedControler.kt
+++ app/src/main/java/dev/jonil/leds_control/LedControler.kt
@@ -7,10 +7,21 @@ import android.content.Context
 import android.content.Intent
 import android.util.Log
 import android.widget.RemoteViews
+import com.google.gson.annotations.SerializedName
+import okhttp3.OkHttpClient
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.http.Headers
+import retrofit2.http.GET
 
 class LedControler : AppWidgetProvider() {
 
     var tag = "main"
+
+    var leds = LedControlService()
 
     override fun onUpdate(
         context: Context,
@@ -38,5 +49,66 @@ class LedControler : AppWidgetProvider() {
     override fun onReceive(context: Context?, intent: Intent?) {
         super.onReceive(context, intent)
         Log.d(tag, intent!!.action!!)
+
+        when (intent.action!!) {
+            "ON" -> {
+                Log.d(tag, "On!")
+                sendRequest()
+            }
+            "Off" -> Log.d(tag, "Off!")
+            "RED" -> Log.d(tag, "Red!")
+        }
+    }
+
+    private fun sendRequest() {
+        leds.getSettings() {
+            it?.let {
+                Log.w(tag, String.format("On %d", it.on))
+            }
+        }
+    }
+}
+
+object ServiceBuilder {
+    private val client = OkHttpClient.Builder().build()
+
+    private val retrofit = Retrofit.Builder()
+        .baseUrl("http://leds.local")
+        .addConverterFactory(GsonConverterFactory.create())
+        .client(client)
+        .build()
+
+    fun<T> buildService(service: Class<T>): T{
+        return retrofit.create(service)
+    }
+}
+
+data class LedSettings (
+    @SerializedName("calibration") val calibration: Int?,
+    @SerializedName("global_brightness") val globalBrightness: Int?,
+    @SerializedName("global_brightness_limit") val globalBrightnessLimit: Float?,
+    @SerializedName("on") val on: Float?,
+)
+
+interface LedControlApi {
+    @Headers("Content-Type: application/json")
+    @GET("getsettings")
+    fun getSettings(): Call<LedSettings>
+}
+
+class LedControlService {
+    fun getSettings(onResult: (LedSettings?) -> Unit) {
+        val retrofit = ServiceBuilder.buildService(LedControlApi::class.java)
+        retrofit.getSettings().enqueue(
+            object : Callback<LedSettings> {
+                override fun onFailure(call: Call<LedSettings>, t: Throwable) {
+                    onResult(null)
+                }
+                override fun onResponse(call: Call<LedSettings>, response: Response<LedSettings>) {
+                    val settings = response.body()
+                    onResult(settings)
+                }
+            }
+        )
     }
 }
